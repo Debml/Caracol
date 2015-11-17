e90e5f02ce84976c23fb9ac7be79e0020908b101
Mauro Amarante <mauro.amarante94@gmail.com>
2015-11-16 23:55:31 -0600

    Avance interfaz

---
+++ src/caracol/Interfaz.java
@@ -0,0 +1,21 @@
+package caracol;
+
+import java.io.*;
+import java.util.*;
+
+class Interfaz {
+	public static void main(String[] args) throws IOException {
+		BufferedReader readFile = new BufferedReader (new FileReader ("interfaz.txt"));
+		
+		//String line = readFile.readLine();
+	}
+	
+	/*
+	+ eliminarAlumno(id : Integer)
+	+ iniciarSesion(usuario : Administrador, password : String)
+	+ inscribirAlumno(AlumnoID : Integer, CursoID : Integer)
+	+ modificarAlumno(x : Estudiante)
+	+ registrarAlumno(nombre : String, edad : Integer, telefono : String, email : String, direccion : String)
+	+registrarCurso(nombre : String, tutor : Tutor, cupo : Integer, lugar : String, fecha_inicio : Date, fecha_fin : Date)
+	*/
+}
